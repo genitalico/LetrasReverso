07bcca674cc1af4d3855d7d11e92738ca637e7a0
Galileo Teco <g@80bits.com>
2015-05-22 14:47:07 -0500

    Initial Commit

---
+++ LetrasReverso/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetrasReverso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = Console.ReadLine();
+
+            char[] array = cadena.ToArray();
+
+            Array.Reverse(array);
+            Console.WriteLine(array);
+            Console.WriteLine(cadena.Count(e => e == 'e'));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Programa Escrito en C# Consola.
+
+Ejemplo escrito para el cuestionario de la web
+
+[http://devf.mx/](http://devf.mx/)
+
+Programa que obtiene una cadena por medio de la consola y las ordenas al reves, tambien cuenta cuantas apariciones tiene la letra e en la cadena.
